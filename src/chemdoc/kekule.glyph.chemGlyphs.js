9e1adbbaa458ccba98d70222657ad7adce3c4ba1
partridgejiang <partridge@cpu.edu.cn>
2019-06-23 07:56:41 +0800

    Fix two typos
    (cherry picked from commit 4bd6a66ff9e3639e22cd15abf55d43fca78d4aaa)

--- src/chemdoc/kekule.glyph.chemGlyphs.js
+++ src/chemdoc/kekule.glyph.chemGlyphs.js
@@ -260,7 +260,7 @@ Kekule.Glyph.ReactionArrow = Class.create(Kekule.Glyph.StraightLine,
 /**
  * Electron pushing arrow (usually connected with two bonds or bond/atom) in reaction.
  * @class
- * @augments Kekule.Glyph.PathGlyph
+ * @augments Kekule.Glyph.BaseArc
  */
 Kekule.Glyph.Arc = Class.create(Kekule.Glyph.BaseArc,
 /** @lends Kekule.Glyph.Arc# */
@@ -272,7 +272,7 @@ Kekule.Glyph.Arc = Class.create(Kekule.Glyph.BaseArc,
 /**
  * Electron pushing arrow (usually connected with two bonds or bond/atom) in reaction.
  * @class
- * @augments Kekule.Glyph.PathGlyph
+ * @augments Kekule.Glyph.BaseArc
  */
 Kekule.Glyph.ElectronPushingArrow = Class.create(Kekule.Glyph.BaseArc,
 /** @lends Kekule.Glyph.ElectronPushingArrow# */
